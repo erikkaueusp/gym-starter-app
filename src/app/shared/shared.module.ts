cd6b68cf361c39ad5240eaa4473bfaa3586d45d4
Erik Kaue <erikkaue@gmail.com>
2022-06-01 23:25:05 -0300

    removido

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,16 @@ import { NgModule } from '@angular/core';
 
 import { MaterialsModule } from './materials/materials.module';
 import { PhotoUploadComponent } from './shared/photo-upload/photo-upload.component';
-import { SnackBarMaterialsComponent } from './shared/snack-bar-materials/snack-bar-materials.component';
 
 
 
 @NgModule({
   declarations: [
     PhotoUploadComponent,
-    SnackBarMaterialsComponent
   ],
   imports: [
     MaterialsModule
   ],
-  exports: [PhotoUploadComponent,SnackBarMaterialsComponent]
+  exports: [PhotoUploadComponent]
 })
 export class SharedModule { }
